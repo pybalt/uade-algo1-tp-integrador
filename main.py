7413ab1d27c74773a7c0241e9d44885c87327290
Tomas <tomasliberi@gmail.com>
2024-09-10 02:39:32 -0300

    Introduje un cambio en el "Ingrese algo"

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 databases = { }
 if __name__ == '__main__':
     print('Bienvenidos a la base de datos no relacional de Base Builders!')
-    project_name = input("Ingrese algo")
+    project_name = input("Ingrese algo: ")
     
