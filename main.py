ace116de1ca07ec0ceaa45a19713b92bd3c635f8
Leonel Baltazar Bravo <96897286+pybalt@users.noreply.github.com>
2024-09-10 22:56:11 -0300

    - Refactor get_databases() to return a dictionary instead of a list - Add documentation

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sys
-
-
-def get_databases() -> list:
+def get_databases() -> dict:
     """
     Retrieves a list of databases.
 
@@ -13,19 +10,59 @@ def get_databases() -> list:
 
 
 def create_database(name: str, directory):
+    """
+    Creates a new database with the given name.
+
+    Parameters:
+    - name (str): The name of the database.
+    - directory: The directory where the database will be stored.
+    """
     databases[name] = {}
 
 
 def list_databases(directory) -> None:
+    """
+    Prints the names of the databases in the given directory.
+
+    Parameters:
+    - directory: Where databases are stored.
+
+    Returns:
+    None
+    """
     for db in directory:
         print(db)
 
 
-def access_database(directory): ...
+def access_database(directory): 
+    """
+    Accesses a database in the given directory.
 
+    Parameters:
+    directory (dict): A dictionary containing the databases.
+
+    Returns:
+    Any: The database corresponding to the given name.
+
+    Raises:
+    KeyError: If the database does not exist.
+
+    """
+    database_name = input("Ingrese nombre de base de datos.\n\t--> ")
+    print(f"Accediendo a la base de datos {database_name}")
+    return directory[database_name]
+
+def exit():
+    print("Saliendo del programa...")
 
 def get_choices() -> dict:
-    choices = {"a": list_databases, "b": access_database}
+    """
+    Returns a dictionary of choices.
+
+    Returns:
+        dict: A dictionary containing the choices.
+    """
+    choices = {"a": list_databases, "b": access_database, "exit()": exit}
     return choices
 
 
@@ -36,16 +73,18 @@ def list_choices(choices: dict) -> None:
         "B": "Acceder directamente a una base de datos",
         "exit()": "Salir del programa",
     }
+    
+    assert len(explanations) == len(choices) # For development purposes. Delete before sprint 1.
+
     for key in explanations:
         print(f"\t{key}: {explanations[key]}")
 
 
-def handle_options(choices) -> None:
+def handle_options(choices) -> str:
 
     list_choices(choices)
     user_input = input("Seleccione una opcion\n\t--> ")
-    if user_input == "exit()":
-        exit()
+
     if user_input.lower() in choices:
         choices[user_input.lower()](databases)
 
