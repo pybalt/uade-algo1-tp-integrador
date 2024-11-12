d2f8b80c7afbe84c159c75fd69382cd29a530bdd
Leonel Baltazar Bravo <96897286+pybalt@users.noreply.github.com>
2024-11-12 00:14:05 -0300

    add: implement logging and error/warning message functions with animation

--- console/__init__.py
+++ console/__init__.py
@@ -1,2 +1,35 @@
+import time
+
 def exit() -> None:
-    print("Saliendo del programa...") 
+    print("Saliendo del programa...")
+
+
+def log(*args) -> None:
+    """
+    Prints the provided arguments to the console.
+    It contains logic to print the arguments in a readable format, color,
+    and animation.
+    """
+    for arg in args:
+        print(arg, end="", flush=True)
+        time.sleep(0.5)
+    print()
+
+def error(*args) -> None:
+    """
+    Prints the provided arguments to the console in red.
+    """
+    for arg in args:
+        print("\033[91m", arg, "\033[0m", end="", flush=True)
+        time.sleep(0.5)
+    print()
+
+def warning(*args) -> None:
+    """
+    Prints the provided arguments to the console in yellow.
+    """
+    for arg in args:
+        print("\033[93m", arg, "\033[0m", end="", flush=True)
+        time.sleep(0.5)
+    print()
+
